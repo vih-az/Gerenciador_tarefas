5fc536838ac3df0706c0d7269f5d128ba730610a
Vitoria Azevedo <vazevedo73@gmail.com>
2024-02-08 22:05:59 -0300

    falta bastante coisa

--- js/login.js
+++ js/login.js
@@ -1,23 +1,26 @@
-const button = document.getElementById('login')
+const button = document.getElementById('login');
 
-async function validarLogin(){
-    const email = document.getElementById('email').value
-    const senha = document.getElementById('senha').value
+async function validarLogin() {
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
 
-    if(email === '' || senha === ''){
-        alert('Preencha os campos corretamente...')
-    }else{
-        const users = await fetch('http://localhost:8080/usuario')
+    if (email === '' || senha === '') {
+        alert('preencha os campos corretamente')
+    } else {
+        const users = await fetch('http://localhost:5080/usuario')
         const listUsers = await users.json()
 
-        listUsers.forEach((user)=>{
-            if(email === user.email && senha === user.senha){
-                alert('Usuário cadastrado com sucesso!')
+        listUsers.forEach((user) => {
+            if (email === user.email && senha === user.senha) {
+                alert('Usuario logado com sucesso !!!')
                 window.location.href = '../html/index.html'
+
+                console.log(listUsers)
             }
         })
     }
 }
+
 
 window.onload = () => {
     button.addEventListener('click', validarLogin)
