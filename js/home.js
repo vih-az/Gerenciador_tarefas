d30f64d16b04ecc8318e2b240f43896e06c5e62f
Vitoria <vazevedo73@gmail.com>
2024-02-22 09:26:45 -0300

    .

--- js/home.js
+++ js/home.js
@@ -1,8 +1,18 @@
 'use strict'
 
-const tarefasPendentes = document.getElementById('task-container')
-const tarefasConcluidas = document.getElementById('finalizadas-task-container')
+const tarefasPendentes = document.getElementById('tasks-pendentes')
+const tarefasConcluidas = document.getElementById('tasks-finalizadas')
 
 async function criarTarefa(){
-    
+    const taskContainer = document.createElement('div')
+    taskContainer.classList.add('task-container')
+
+    const imgTarefa = document.createElement('img')
+    imgTarefa.classList.add('task-photo')
+
+    const tituloTarefa = document.createElement('h4')
+    tituloTarefa.classList.add('task-name')
+
+    const descricaoTarefa = document.createElement('h5')
+    descricaoTarefa.classList.add('task-description')
 }
