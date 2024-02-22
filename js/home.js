2657c0f0de2992c61622439a2aec4a82f5eb62a7
Vitoria <vazevedo73@gmail.com>
2024-02-22 11:14:14 -0300

    .

--- js/home.js
+++ js/home.js
@@ -2,6 +2,7 @@
 
 const tarefasPendentes = document.getElementById('tasks-pendentes')
 const tarefasConcluidas = document.getElementById('tasks-finalizadas')
+const idUsuario = localStorage.getItem('idUsuario')
 
 async function criarTarefa(){
     const taskContainer = document.createElement('div')
@@ -9,10 +10,19 @@ async function criarTarefa(){
 
     const imgTarefa = document.createElement('img')
     imgTarefa.classList.add('task-photo')
+    imgTarefa.src('../img/image 10.png')
 
     const tituloTarefa = document.createElement('h4')
     tituloTarefa.classList.add('task-name')
 
     const descricaoTarefa = document.createElement('h5')
     descricaoTarefa.classList.add('task-description')
+}
+
+async function validarTarefa(){
+    const url = 'http://localhost:5080/tarefas'
+    const response = await fetch(url)
+    const listaTarefas = response.json()
+    
+    
 }
