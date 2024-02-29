514adb470d67265b6405ee6674a56c23d48e568d
Vitoria <vazevedo73@gmail.com>
2024-02-29 12:25:54 -0300

    .

--- js/home.js
+++ js/home.js
@@ -3,6 +3,7 @@
 const tarefasPendentes = document.getElementById('tasks-pendentes')
 const tarefasConcluidas = document.getElementById('tasks-finalizadas')
 const idUser = localStorage.getItem('idUsuario')
+const iconeDeletarTarefa = document.createElement('img')
 
 async function criarContainerTarefa(tarefa) {
     const taskContainer = document.createElement('div')
@@ -10,7 +11,11 @@ async function criarContainerTarefa(tarefa) {
 
     const imgTarefa = document.createElement('img')
     imgTarefa.classList.add('task-photo')
-    imgTarefa.src = `../img/${tarefa.foto}`
+    imgTarefa.src = `../img/image 10.png`
+
+    if (!imgTarefa) {
+        imgTarefa.src = 'https://img.icons8.com/?size=80&id=45368&format=png'
+    }
 
     const tituloTarefa = document.createElement('h4')
     tituloTarefa.classList.add('task-name')
@@ -20,19 +25,51 @@ async function criarContainerTarefa(tarefa) {
     descricaoTarefa.classList.add('task-description')
     descricaoTarefa.textContent = tarefa.descricao
 
-    taskContainer.appendChild(imgTarefa, tituloTarefa, descricaoTarefa)
+    const iconeDeletarTarefa = document.createElement('img')
+    iconeDeletarTarefa.setAttribute('id', 'deletarTarefa')
+    iconeDeletarTarefa.src = 'https://img.icons8.com/?size=30&id=ypOzn2c4IIbr&format=png'
+    iconeDeletarTarefa.addEventListener('click', deletarTarefa(tarefa.id))
+    taskContainer.append(imgTarefa, tituloTarefa, descricaoTarefa, iconeDeletarTarefa)
+    return taskContainer
 }
 
 async function validarTarefa() {
     const url = 'http://localhost:5080/tarefas'
     const response = await fetch(url)
-    const listaTarefas = response.json()
-
+    const listaTarefas = await response.json()
     const listaTarefasARRAY = []
-    listaTarefas.forEach(function(tarefa){
-        if(idUser==tarefa.idUsuario){
+    listaTarefas.forEach(async function (tarefa) {
+        if (idUser == tarefa.idUsuario) {
             listaTarefasARRAY.push(tarefa)
-            criarContainerTarefa(listaTarefasARRAY)
         }
     })
+    return listaTarefasARRAY
 }
+
+async function mostrarContainerTarefa() {
+
+    const tarefa = await validarTarefa()
+    tarefa.forEach(async function (tarefa) {
+
+        const containerTarefa = document.querySelector("div.tasks-pendentes")
+        const criarTarefa = await criarContainerTarefa(tarefa)
+        containerTarefa.appendChild(criarTarefa)
+    })
+}
+
+async function deletarTarefa(tarefa) {
+    const iconeDeletarTarefa = document.getElementById('deletarTarefa')
+    try {
+        const url = `http://localhost:5080/tarefas${tarefa}`
+        const options = {
+            method: 'DELETE'
+        }
+        await fetch(url, options)
+        console.log('tarefa deletada com sucesso !!')
+        // window.location.reload()
+    }catch(error){
+        alert('não foi possível deletar a tarefa !!')
+    }
+}
+// iconeDeletarTarefa.addEventListener('click', deletarTarefa(tarefa))
+mostrarContainerTarefa()
